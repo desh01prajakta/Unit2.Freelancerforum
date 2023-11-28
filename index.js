5b06627b802e01dcdf2b984aedbd8fc40abbc6bb
desh01prajakta <prajudeshmukh11@gmail.com>
2023-11-28 17:11:09 -0600

    adds value

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 
+const freeLancers = ["Alice", "Bob", "Carol", "Eva"]
+const Occupation = ["Writer", "Teacher", "singer", "Dancer", "programmer"];
+const startingPrice = [50, 70];
 
-
-
-const freeLancer = ["Alice", "Bob"];
-const Occupation = ["Writer", "Teacher"];
-const number = [50, 30];
-
-function getAvg(number){
+function getAvg(startingPrice){
 let sum = 0;
-for(let i = 0; i < number.length; i++){
-    sum += number[i];
+for(let i = 0; i < startingPrice.length; i++){
+    sum += startingPrice[i];
 } 
-return sum
+return sum/startingPrice.length
 }
-getAvg()
+console.log(getAvg(startingPrice))
+
+const freeLancer = document.getElementById ("freeLancer")
+
+const initialFreeLancer = [{name: "Carol", occupation: "Programmer", startingPrice: 50},{name:"Eva", occupation:"singer",startingPrice:70}];
+for(let i = 0; i < initialFreeLancer.length; i++){
+
+const row = freeLancer.insertRow(1);
+const cell = row.insertCell(0);
+const cell1 = row.insertCell(1);
+const cell2 = row.insertCell(2);
+cell1.innerHTML = initialFreeLancer[i].occupation;
+cell.innerHTML = initialFreeLancer [i].name;
+cell2.innerHTML = initialFreeLancer [i].startingPrice;
+}
+getAvg([50,70])
