2b6c94b24751ae99646a5e1da2a62d5263388bdf
desh01prajakta <prajudeshmukh11@gmail.com>
2023-11-27 19:30:28 -0600

    adds values

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+
+
+const freeLancer = ["Alice", "Bob"];
+const Occupation = ["Writer", "Teacher"];
+const number = [50, 30];
+
+function getAvg(number){
+let sum = 0;
+for(let i = 0; i < number.length; i++){
+    sum += number[i];
+} 
+return sum
+}
+getAvg()
